cba392ce247d9e4d9359ea7ebc1940d03391a009
gittjar <jarno.keranen@gmail.com>
2023-05-25 21:00:31 +0300

    station model and controller added, linked to index

--- MVCCitybike/Models/Station.cs
+++ MVCCitybike/Models/Station.cs
@@ -3,9 +3,20 @@ namespace MVCCitybike.Models
 {
 	public class Station
 	{
-		public Station()
-		{
-		}
-	}
+        public int FID { get; set; }
+        public int ID { get; set; }
+        public string? Nimi { get; set; }
+        public string? Namn { get; set; }
+        public string? Name { get; set; }
+        public string? Osoite { get; set; }
+        public string? Adress { get; set; }
+        public string? Kaupunki { get; set; }
+        public string? Stad { get; set; }
+        public string? Operaattor { get; set; }
+        public int Kapasiteet { get; set; }
+        public decimal x { get; set; }
+        public decimal y { get; set; }
+
+    }
 }
 
