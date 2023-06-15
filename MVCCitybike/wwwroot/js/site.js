a15498c7c78c5843175a54962730dd680c78a25b
gittjar <jarno.keranen@gmail.com>
2023-06-15 14:44:13 +0300

    small layout updates and javascript date and clock to navbar

--- MVCCitybike/wwwroot/js/site.js
+++ MVCCitybike/wwwroot/js/site.js
@@ -29,9 +29,54 @@ function hide() {
         clearInterval(intervalID);
     }
 }
-// User IP address
+// User IP address in indexpage
 $.getJSON("https://api.ipify.org?format=json", function (data) {
 
     // Setting text of element
     $("#yourip").html(data.ip);
 })
+// clock
+function updateClock() {
+    const now = new Date();
+
+    // Format time as "hh:mm:ss"
+    const hours = String(now.getHours()).padStart(2, '0');
+    const minutes = String(now.getMinutes()).padStart(2, '0');
+    const seconds = String(now.getSeconds()).padStart(2, '0');
+    const time = `${hours}:${minutes}:${seconds}`;
+
+    // Format date as "dd.mm.yyyy"
+    const year = now.getFullYear();
+    const month = String(now.getMonth() + 1).padStart(2, '0');
+    const day = String(now.getDate()).padStart(2, '0');
+    const date = `${day}.${month}.${year}`;
+
+    // Update the clock and date elements
+    document.getElementById('clock').textContent = time;
+    document.getElementById('date').textContent = date;
+}
+
+// Call updateClock() initially to display the current time and date
+updateClock();
+
+// Update the clock every second
+setInterval(updateClock, 1000);
+
+// Fontawesome icons
+function createIcons() {
+    const iconContainer = document.getElementById('icon-container');
+
+    // Create the arrow-left icon
+    const arrowLeftIcon = document.createElement('i');
+    arrowLeftIcon.classList.add('fas', 'fa-arrow-left');
+    iconContainer.appendChild(arrowLeftIcon);
+
+    // Create the arrow-right icon
+    const arrowRightIcon = document.createElement('i');
+    arrowRightIcon.classList.add('fas', 'fa-arrow-right');
+    iconContainer.appendChild(arrowRightIcon);
+}
+
+// Call the createIcons function to generate the icons
+createIcons();
+
--- readme.txt
+++ readme.txt
@@ -1,12 +1,16 @@
-## MVC Citybike CRUD ##
+<< MVC Citybike CRUD >>
 
 - Stations
 - Biketrips
 
 Features:
 - Create stations / trips
+- Read stations / trips
 - Update stations / trips
 - Delete stations / trips
 - Sort data by departure time or return time in trips section
+- Search stations by cities
+- Show user IP address when entering to site
+- Show today date and current time
 
 Connected to Azure SQL.
