b117ec01b0894ce6936323254447be0d111ad0c4
gittjar <jarno.keranen@gmail.com>
2024-12-30 20:05:52 +0200

    added requirements for input fields

--- MVCCitybike/Models/BiketripsMay2021.cs
+++ MVCCitybike/Models/BiketripsMay2021.cs
@@ -1,8 +1,6 @@
 ﻿using System;
 using System.ComponentModel.DataAnnotations;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.VisualStudio.Web.CodeGeneration;
-using static Microsoft.EntityFrameworkCore.DbLoggerCategory;
 
 namespace MVCCitybike.Models
 {
@@ -10,20 +8,31 @@ namespace MVCCitybike.Models
     {
         [Key]
         public int ID { get; set; }
+        
+        [Required(ErrorMessage = "Departure is required")]
         public DateTime Departure { get; set; }
+        
+        [Required(ErrorMessage = "Return is required")]
         public DateTime Return { get; set; }
+        
+        [Required(ErrorMessage = "Departure station ID is required")]
         public int Departure_station_id { get; set; }
+        
+        [Required(ErrorMessage = "Departure station name is required")]
         public string? Departure_station_name { get; set; }
+        
+        [Required(ErrorMessage = "Return station ID is required")]
         public int Return_station_id { get; set; }
+        
+        [Required(ErrorMessage = "Return station name is required")]
         public string? Return_station_name { get; set; }
+        
+        [Required(ErrorMessage = "Covered distance is required")]
+        [Range(0, double.MaxValue, ErrorMessage = "Covered distance must be a positive number")]
         public decimal Covered_distance_m { get; set; }
+        
+        [Required(ErrorMessage = "Duration is required")]
+        [Range(0, int.MaxValue, ErrorMessage = "Duration must be a positive number")]
         public int Duration_sec { get; set; }
     }
-    // Add context and controller
-    // dotnet aspnet-codegenerator controller -name BiketripsMay2021Controller -m BiketripsMay2021 -dc MvcBiketripsMay2021.Data.MvcBiketripsMay2021Context --relativeFolderPath Controllers --useDefaultLayout --referenceScriptLibraries -sqlite
-    // export PATH=$HOME/.dotnet/tools:$PATH
-    // dotnet ef migrations add InitialCreate --context MvcBiketripsMay2021Context
-    //
-    // Change in Program.cs right connection string!
 }
-
--- MVCCitybike/Models/Station.cs
+++ MVCCitybike/Models/Station.cs
@@ -8,33 +8,50 @@ namespace MVCCitybike.Models
 {
     public class Station
     {
-
         public int FID { get; set; }
+        
         [Key]
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
-        // autoupdate increase +1 id
         public int ID { get; set; }
+        
         [Comment("Citybike aseman nimi")]
-        [Required]
+        [Required(ErrorMessage = "Nimi is required")]
         public string? Nimi { get; set; }
-        public string? Namn { get; set; }
-        public string? Name { get; set; }
-        [Required]
+        
+        [Required(ErrorMessage = "Osoite is required")]
         public string? Osoite { get; set; }
-        public string? Adress { get; set; }
-        [Required]
+        
+        [Required(ErrorMessage = "Kaupunki is required")]
         public string? Kaupunki { get; set; }
-        public string? Stad { get; set; }
-        public string? Operaattor { get; set; }
-        public int Kapasiteet { get; set; }
-        [Required]
+        
+        [Required(ErrorMessage = "x is required")]
         [Column(TypeName = "decimal(8, 6)")]
         public decimal x { get; set; }
-        [Required]
+        
+        [Required(ErrorMessage = "y is required")]
         [Column(TypeName = "decimal(9, 6)")]
         public decimal y { get; set; }
-        // kuvatiedosto
+        
+        [Comment("Citybike aseman nimi ruotsiksi")]
+        public string? Namn { get; set; }
+        
+        [Comment("Citybike station name in English")]
+        public string? Name { get; set; }
+        
+        [Comment("Citybike aseman osoite ruotsiksi")]
+        public string? Adress { get; set; }
+        
+        [Comment("Citybike aseman kaupunki ruotsiksi")]
+        public string? Stad { get; set; }
+        
+        [Comment("Citybike aseman operaattori")]
+        public string? Operaattor { get; set; }
+        
+        [Comment("Citybike aseman kapasiteetti")]
+        public int Kapasiteet { get; set; }
+
+        [Comment("Citybike aseman kuva")]
+        [Required(ErrorMessage = "Kuva is required")]
         public string? Kuva { get; set; }
     }
 }
-
